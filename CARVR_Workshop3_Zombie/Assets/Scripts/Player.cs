c1efc2d96b87ad4b94cb22b83fc9bb3681b76a5e
Nolan Scobie <nolan.scobie@gmail.com>
2019-09-23 23:55:34 -0400

    Zombiessss

---
+++ CARVR_Workshop3_Zombie/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+
+    // Note: this assumes we will only ever have one player in our game, and would break if we wanted to add multiplayer :(
+    public static Player Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ CARVR_Workshop3_Zombie/Assets/Scripts/Shooter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] private KeyCode triggerKey;
+    [SerializeField] private float maxDistance = 30f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
