6ff1387687e762c357f0fec161e2f987889b06f9
Brian Faggin Batista <brianfbatista@gmail.com>
2019-09-24 01:17:40 -0400

    almost done
    Need to add:
    Eyes to the zombies
    Change the floor material (less metalic, more bright)
    SFX / Music???? Please???

--- CARVR_Workshop3_Zombie/Assets/Scripts/eventMasterScript.cs
+++ CARVR_Workshop3_Zombie/Assets/Scripts/eventMasterScript.cs
@@ -16,6 +16,6 @@ public class eventMasterScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+    
     }
 }
---
+++ CARVR_Workshop3_Zombie/Assets/Scripts/uiController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class uiController : MonoBehaviour
+{
+    [SerializeField]
+    private Button startButton;
+
+
+    [SerializeField]
+    private GameObject openingPanel;
+
+    private void Awake()
+    {
+        startButton.onClick.AddListener(ChangeScene);   
+    }
+
+    private void ChangeScene()
+    {
+        openingPanel.SetActive(false);
+        SceneManager.LoadScene("ZombieScene");
+    }
+}
